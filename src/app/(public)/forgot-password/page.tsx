3c42a4c13d6712b0bb2f712dac6d6f53c7e0b7f8
quyentph <quyentph@gmail.com>
2026-09-08 16:10:04 +0700

    fix layout reset password

--- src/app/(public)/forgot-password/page.tsx
+++ src/app/(public)/forgot-password/page.tsx
@@ -10,16 +10,18 @@ import Container from './_components/container';
 
 const ForgotPassword = () => {
   return (
-    <div className="w-full m-auto md:h-auto md:w-[95%] md:max-w-300 bg-white md:border md:border-gray-300 md:rounded-lg md:shadow-lg md:shadow-gray-200 p-6 md:p-10 overflow-y-auto md:overflow-hidden">
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-4 lg:gap-12">
-        {/* Cột trái: Quên mật khẩu */}
-        <Container />
+    <div className="min-h-screen w-full flex items-center justify-center bg-white md:bg-gray-50/30 p-0 md:p-4 sm:p-6 lg:p-8">
+      <div className="w-full min-h-screen md:min-h-fit md:w-[95%] md:max-w-300 bg-white md:border md:border-gray-200 md:rounded-2xl md:shadow-xl md:shadow-gray-200/50 p-5 sm:p-6 md:p-10 overflow-y-auto md:overflow-hidden flex flex-col justify-center">
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 lg:gap-12">
+          {/* Cột trái: Quên mật khẩu */}
+          <Container />
 
-        {/* Cột phải: Minh họa hệ thống */}
-        <div className="hidden md:flex col-span-12 lg:col-span-7 flex-col gap-8 lg:pl-2 justify-between">
-          <Hero />
-          <StatCard />
-          <PreviewSystem />
+          {/* Cột phải: Minh họa hệ thống */}
+          <div className="hidden md:flex col-span-12 lg:col-span-7 flex-col gap-8 lg:pl-2 justify-between">
+            <Hero />
+            <StatCard />
+            <PreviewSystem />
+          </div>
         </div>
       </div>
     </div>
